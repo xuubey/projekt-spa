124c0992a3bee725c197fdcebc91d80b23726da1
xuubey <krasny.sylwia@gmail.com>
2020-06-21 13:02:25 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import './App.css'
+import {BrowserRouter as Router, NavLink, Route, Switch} from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Home = () => <h1>Strona startowa</h1>;
+const News = () => <h1>Aktualności</h1>;
+const Contact = () => <h1>Kontakt</h1>;
+
+const ErrorPage = () => <h1>Strona nie istnieje!</h1>
+
+class App extends Component {
+    render() {
+        return (
+            <Router>
+                <div>
+                    <header>
+                        <nav>
+                            <ul>
+                                <li><NavLink to="/" exact activeClassName={"home_selected"}>Start</NavLink></li>
+                                <li><NavLink to="/news" activeClassName={"news_selected"}>Aktualności</NavLink></li>
+                                <li><NavLink to="/contact" activeClassName={"contact_selected"}>Kontakt</NavLink></li>
+                            </ul>
+                        </nav>
+                    </header>
+                    <section>
+                        <Switch>
+                        <Route path="/" exact component={Home}/>
+                        <Route path="/news" component={News}/>
+                        <Route path="/contact" component={Contact}/>
+                        <Route component={ErrorPage}/>
+                        </Switch>
+                    </section>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
